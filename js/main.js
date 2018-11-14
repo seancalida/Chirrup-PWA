1cedd0f4aa5ac3142af027370e2a1f40781419dd
Sean Calida <seanc830@vt.edu>
2018-11-13 23:38:04 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+function openSettings() {
+  document.getElementById("settingsID").style.width = "100%";
+}
+
+function exitSettings() {
+  document.getElementById("settingsID").style.width = "0";
+}
+
+function openSend() {
+  document.getElementById("sendID").style.height = "100%";
+}
+
+function exitSend() {
+  document.getElementById("sendID").style.height = "0";
+}
+
+function dropColorOptions() {
+  document.getElementById("ddc").style.display = "block";
+}
+
+function colorBlue() {
+  document.getElementById("color_value").innerHTML = "Blue";
+  document.getElementById("ddc").style.display = "none";
+}
+
+function colorRed() {
+  document.getElementById("color_value").innerHTML = "Red";
+  document.getElementById("ddc").style.display = "none";
+}
+function colorYellow() {
+  document.getElementById("color_value").innerHTML = "Yellow";
+  document.getElementById("ddc").style.display = "none";
+}
+function colorGreen() {
+  document.getElementById("color_value").innerHTML = "Green";
+  document.getElementById("ddc").style.display = "none";
+}
+
+function dropRhythymOptions() {
+  document.getElementById("ddr").style.display = "block";
+}
+
+function rhythym1() {
+  document.getElementById("rhythym_value").innerHTML = "Rhythym 1";
+  document.getElementById("ddr").style.display = "none";
+}
+
+function rhythym2() {
+  document.getElementById("rhythym_value").innerHTML = "Rhythym 2";
+  document.getElementById("ddr").style.display = "none";
+}
+function rhythym3() {
+  document.getElementById("rhythym_value").innerHTML = "Rhythym 3";
+  document.getElementById("ddr").style.display = "none";
+}
+function rhythym4() {
+  document.getElementById("rhythym_value").innerHTML = "Rhythym 4";
+  document.getElementById("ddr").style.display = "none";
+}
